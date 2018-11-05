0b4ff2dbe1579243bcf4169baa5742f18b1de6f4
hexoul <crosien@gmail.com>
2018-11-05 16:52:14 +0900

    add chart library and render sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
 import React from 'react';
 import { Layout } from 'antd';
+import { Line } from 'react-chartjs-2';
 
 // Styles.
 import './App.css';
 
+// Raw data
 import data from './report.log';
 
+function getChartData() {
+  return {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+      {
+        label: 'My Second dataset',
+        fillColor: 'rgba(151,187,205,0.2)',
+        strokeColor: 'rgba(151,187,205,1)',
+        pointColor: 'rgba(151,187,205,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(151,187,205,1)',
+        data: [28, 48, 40, 19, 86, 27, 90],
+      },
+    ]
+  }
+}
+
+const options = {
+  scaleShowGridLines: true,
+  scaleGridLineColor: 'rgba(0,0,0,.05)',
+  scaleGridLineWidth: 1,
+  scaleShowHorizontalLines: true,
+  scaleShowVerticalLines: true,
+  bezierCurve: true,
+  bezierCurveTension: 0.4,
+  pointDot: true,
+  pointDotRadius: 4,
+  pointDotStrokeWidth: 1,
+  pointHitDetectionRadius: 20,
+  datasetStroke: true,
+  datasetStrokeWidth: 2,
+  datasetFill: true,
+  legendTemplate: '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+}
+
 class App extends React.Component {
+  
+  data = {
+    origin: [],
+    lineChart: {},
+  };
+
+  state = {
+    ready: false,
+  }
 
   async loadRawData() {
     let resp = await fetch(data);
     let ret = await resp.text();
     ret.split('\n').forEach(line => {
       if (! line) return;
-      console.log(JSON.parse(line));
+      this.data.origin.push(JSON.parse(line));
     });
+    
+    console.log(this.data.origin);
+    this.data.lineChart = getChartData();
+    this.setState({ ready: true });
   }
 
-  componentWillMount() {
+  constructor() {
+    super();
     this.loadRawData();
   }
 
@@ -28,9 +90,14 @@ class App extends React.Component {
           Header
         </Layout.Header>
         <Layout.Content style={{ padding: '5vh 5vw 0vh 5vw', backgroundColor: '#fff' }}>
-          Content
+          <Line
+            data={this.data.lineChart}
+            options={options}
+            width="600" height="250"
+          />
         </Layout.Content>
         <Layout.Footer>
+          <a href={data}>Raw data</a>
           <center>coinmarketcap-graph ©2018 Created by hexoul</center>
         </Layout.Footer>
       </Layout>
